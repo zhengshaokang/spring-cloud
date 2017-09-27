a34941db1556591e3a3a47fd0de07ddfe37278a9
郑绍康 <2851823541@qq.com>
2017-09-27 14:41:51 +0800

    u

--- zsk-service/src/main/java/com/zsk/service/redis/config/RedisConfig.java
+++ zsk-service/src/main/java/com/zsk/service/redis/config/RedisConfig.java
@@ -42,7 +42,7 @@ public class RedisConfig extends CachingConfigurerSupport{
         return template;
     }
     private void setSerializer(StringRedisTemplate template) {
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+		Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
